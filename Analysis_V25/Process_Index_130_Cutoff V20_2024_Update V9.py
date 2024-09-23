d8ad4e42b37b12ac683167d17ace20d19338796c
lbabbi@qontigo.com <lbabbi@qontigo.com>
2024-09-23 23:27:54 +0200

    "V29"

---
+++ Analysis_V25/Process_Index_130_Cutoff V20_2024_Update V9.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from datetime import datetime
+import os
+
+Version = "V9"
+Step_2 = True
+
+# Read V25 Composition
+V25 = pd.read_csv(r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\V20_SAMCO\Output\Close\2024\V5\Sharable_Final_Buffer_V20_Cutoff_Mcap_Enhanced_STEP2_Close_V5.csv",
+                    parse_dates=["Date"], sep=",", index_col=0)[["Date", "ISIN", "SEDOL", "Internal_Number", "Country", "Weight"]]
+
+# Capfactor from SWACALLCAP
+CapFactor = pd.read_csv(r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\V20_SAMCO\Universe\Capfactor_SWACALLCAP_MAR2019.csv", parse_dates=["Date"], index_col=0)
+
+# Create weightFactor
+V25["weightFactor"] = 1
+
+Output = pd.DataFrame(columns=["Date", "CapFactor_Adjustment"])
+
+for date in V25.Date.unique():
+    temp_V25 = V25.query("Date == @date")
+
+    # Initialize CF_Adjustment with None (or 1.0 if you prefer a default value)
+    CF_Adjustment = None
+
+    # Check if total weight for CN is > 10%
+    if temp_V25.query("Country == 'CN'")["Weight"].sum() > 0.10:
+        # Calculate CapFactor Adjustment
+        CF_Adjustment = (0.10 * temp_V25.query("Country != 'CN'")["Weight"].sum()) / (0.90 * temp_V25.query("Country == 'CN'")["Weight"].sum())
+
+    else:
+        CF_Adjustment = 1
+
+    # Create a temporary DataFrame for the current date
+    temp_Output = pd.DataFrame([{
+        "Date": date,
+        "CapFactor_Adjustment": CF_Adjustment
+    }])
+    
+    # Append temp_Output to the main Output DataFrame
+    Output = pd.concat([Output, temp_Output], ignore_index=True)
+
+# Create the iStudio Output
+# Add CapFactor from SWACALLCAP
+V25 = V25.merge(CapFactor[["Date", "Internal_Number", "Capfactor"]], on=["Date", "Internal_Number"], how="left")
+
+Final_Output = pd.DataFrame()
+
+# Adjust the CN CapFactor
+for date in V25.Date.unique():
+    temp_V25 = V25.query("Date == @date")
+
+    # Multiply the existing Capfactor by the corresponding CapFactor_Adjustment from Output
+    temp_V25.loc[temp_V25["Country"] == "CN", "Capfactor"] *= Output.loc[Output["Date"] == date, "CapFactor_Adjustment"].values[0]
+
+    Final_Output = pd.concat([Final_Output, temp_V25])
+
+# Convert column Date
+Final_Output["Date"] = Final_Output["Date"].dt.strftime('%Y%m%d')
+
+# Renaming to convention
+Final_Output = Final_Output.rename(columns={"Internal_Number": "STOXXID", "Date": "effectiveDate", "Capfactor": "capFactor"})
+
+# Store the .CSV
+# Get current date formatted as YYYYMMDD_HHMMSS
+current_datetime = datetime.today().strftime('%Y%m%d')
+
+# Store the .CSV with version and timestamp
+Final_Output[["STOXXID", "SEDOL", "ISIN", "effectiveDate", "weightFactor", "capFactor"]].to_csv(
+    os.path.join(
+        r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\V20_SAMCO\iStudio\Output", 
+        str(Version) + "_" + current_datetime + "_STEP_2" + str(Step_2) +".csv"
+    ), 
+    index=False, 
+    lineterminator="\n", 
+    sep=";"
+)
--- iStudio/Create_Input.py
+++ iStudio/Create_Input.py
@@ -2,8 +2,10 @@ import pandas as pd
 import os
 from datetime import datetime
 
-Versions = ["V6"]
+Versions = ["V9"]
 Price = "Close"
+
+Step_2 = True
 
 Path = r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\V20_SAMCO\Output"
 
@@ -13,36 +15,79 @@ CapFactor = pd.read_csv(r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\
 # Read each versions
 for Version in Versions:
 
-    if Version == "V1":
-        CSV = os.path.join(Path, Price, "2024", Version, "Sharable_Final_Buffer_V20_Cutoff_Mcap_Enhanced_STEP2_Close_V1.csv")
-    else:
-        CSV = os.path.join(Path, Price, "2024", Version, "Sharable_Final_Buffer_V20_Cutoff_Mcap_Enhanced_STEP2_Close_" + Version + ".csv")
+    # Check if STEP 2 is implemented
+    if Step_2 == True:
 
-    Frame = pd.read_csv(CSV, index_col=0, parse_dates=["Date"])
-    
-    # Filter for needed columns
-    Frame = Frame[["Internal_Number", "SEDOL", "ISIN", "Date"]]
-    # Create weightFactor
-    Frame["weightFactor"] = 1
+        if Version == "V1":
+            CSV = os.path.join(Path, Price, "2024", Version, "Sharable_Final_Buffer_V20_Cutoff_Mcap_Enhanced_STEP2_Close_V1.csv")
+        elif Version == "V8": # Version 8 is based on Version 5 but with adjustment for CapFactor
+            Version = "V5"
+            CSV = os.path.join(Path, Price, "2024", Version, "Sharable_Final_Buffer_V20_Cutoff_Mcap_Enhanced_STEP2_Close_" + Version + ".csv")
+            Version = "V8"
+        elif Version == "V9": # Version 8 is based on Version 5 but with adjustment for CapFactor
+            Version = "V1"
+            CSV = os.path.join(Path, Price, "2024", Version, "Sharable_Final_Buffer_V20_Cutoff_Mcap_Enhanced_STEP2_Close_" + Version + ".csv")
+            Version = "V9"
+        else:
+            CSV = os.path.join(Path, Price, "2024", Version, "Sharable_Final_Buffer_V20_Cutoff_Mcap_Enhanced_STEP2_Close_" + Version + ".csv")
 
-    # Add CapFactor from SWACALLCAP
-    Frame = Frame.merge(CapFactor[["Date", "Internal_Number", "Capfactor"]], on=["Date", "Internal_Number"], how="left")
+        Frame = pd.read_csv(CSV, index_col=0, parse_dates=["Date"])
+        
+        # Filter for needed columns
+        Frame = Frame[["Internal_Number", "SEDOL", "ISIN", "Date", "Country"]]
+        # Create weightFactor
+        Frame["weightFactor"] = 1
 
-    # Convert column Date
-    Frame["Date"] = Frame["Date"].dt.strftime('%Y%m%d')
+        # Add CapFactor from SWACALLCAP
+        Frame = Frame.merge(CapFactor[["Date", "Internal_Number", "Capfactor"]], on=["Date", "Internal_Number"], how="left")
 
-    # Renaming to convention
-    Frame = Frame.rename(columns={"Internal_Number": "STOXXID", "Date": "effectiveDate", "Capfactor": "capFactor"})
+        # Specific V8 case
+        if Version == "V8" or Version == "V9":
+            Frame.loc[~Frame["Country"].isin(["CN", "IN", "ID", "KR", "PH", "QA", "SA", "TW", "TH", "AE"]), "Capfactor"] = 1
 
-    # Store the .CSV
-    # Get current date formatted as YYYYMMDD_HHMMSS
-    current_datetime = datetime.today().strftime('%Y%m%d')
+        # Drop Country columns
+        Frame = Frame.drop(columns={"Country"})
+
+        # Convert column Date
+        Frame["Date"] = Frame["Date"].dt.strftime('%Y%m%d')
+
+        # Renaming to convention
+        Frame = Frame.rename(columns={"Internal_Number": "STOXXID", "Date": "effectiveDate", "Capfactor": "capFactor"})
+
+        # Store the .CSV
+        # Get current date formatted as YYYYMMDD_HHMMSS
+        current_datetime = datetime.today().strftime('%Y%m%d')
+
+    else: # STEP 2 is not implemented
+
+        CSV = os.path.join(Path, Version + "_Base", "Final_Buffer_V20_Cutoff_Mcap_Enhanced_2024.csv")
+
+        Frame = pd.read_csv(CSV, index_col=0, parse_dates=["Date"]).query("Date >= '2019-03-18'") # Filter for Dates as STEP 1 starts in 1997
+
+                # Filter for needed columns
+        Frame = Frame[["Internal_Number", "SEDOL", "ISIN", "Date"]]
+        # Create weightFactor
+        Frame["weightFactor"] = 1
+
+        # Add CapFactor from SWACALLCAP
+        Frame = Frame.merge(CapFactor[["Date", "Internal_Number", "Capfactor"]], on=["Date", "Internal_Number"], how="left")
+
+        # Convert column Date
+        Frame["Date"] = Frame["Date"].dt.strftime('%Y%m%d')
+
+        # Renaming to convention
+        Frame = Frame.rename(columns={"Internal_Number": "STOXXID", "Date": "effectiveDate", "Capfactor": "capFactor"})
+
+        # Store the .CSV
+        # Get current date formatted as YYYYMMDD_HHMMSS
+        current_datetime = datetime.today().strftime('%Y%m%d')
+
 
     # Store the .CSV with version and timestamp
     Frame.to_csv(
         os.path.join(
             r"C:\Users\lbabbi\OneDrive - ISS\Desktop\Projects\SAMCO\V20_SAMCO\iStudio\Output", 
-            str(Version) + "_" + current_datetime + ".csv"
+            str(Version) + "_" + current_datetime + "_STEP_2" + str(Step_2) +".csv"
         ), 
         index=False, 
         lineterminator="\n", 
